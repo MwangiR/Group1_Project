ee2353e9d5f8b41ad795d4df72e14ff025644b52
AlexanderDuncan1 <hellstern@gmail.com>
2023-07-04 15:12:34 +1000

    acessuri

--- scripts/index.js
+++ scripts/index.js
@@ -1,84 +1,82 @@
+var clientId = '07574a44bbef47ad9c5b4949cf020c29';
+var redirectUri = 'https://alexanderduncan1.github.io/Group1_Project/';
+
 function authenticate() {
-    const clientId = '07574a44bbef47ad9c5b4949cf020c29';
-    const clientSecret = 'Zander85';
-  
-    const authEndpoint = 'https://accounts.spotify.com/authorize';
-    const redirectUri = 'https://alexanderduncan1.github.io/Group1_Project/';
-    const scopes = 'user-library-read playlist-read-private';
-  
-    // Redirect the user to Spotify's authorization endpoint
-    window.location.href = `${authEndpoint}?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
+  var state = generateRandomString(16);
+  localStorage.setItem('spotify_auth_state', state);
+
+  // Construct the authorization URL
+  var authorizeUrl = 'https://accounts.spotify.com/authorize?' +
+    'response_type=code' +
+    '&client_id=' + clientId +
+    '&redirect_uri=' + encodeURIComponent(redirectUri) +
+    '&state=' + state;
+
+  // Redirect the user to Spotify's authorization endpoint
+  window.location.href = authorizeUrl;
+}
+
+// Handle the callback logic
+function handleCallback() {
+  // Extract the query parameters from the callback URL
+  var query = window.location.search.substring(1);
+  var params = new URLSearchParams(query);
+
+  // Retrieve the authorization code and state from the query parameters
+  var code = params.get('code');
+  var state = params.get('state');
+
+  // Verify the state to prevent CSRF attacks
+  var storedState = localStorage.getItem('spotify_auth_state');
+  if (state === null || state !== storedState) {
+    console.error('Invalid state parameter.');
+    return;
   }
-  
-  // Handle the redirect URI after the user grants permission
-  window.addEventListener('load', function() {
-    const code = new URLSearchParams(window.location.search).get('code');
-    if (code) {
-      const clientId = '07574a44bbef47ad9c5b4949cf020c29';
-      const clientSecret = 'Zander85';
-      const tokenEndpoint = 'https://accounts.spotify.com/api/token';
-      const redirectUri = 'https://alexanderduncan1.github.io/Group1_Project/';
-  
-      // Exchange the authorization code for an access token
-      fetch(tokenEndpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
-        },
-        body: new URLSearchParams({
-          'grant_type': 'authorization_code',
-          'code': code,
-          'redirect_uri': redirectUri
-        })
-      })
-      .then(response => response.json())
-      .then(data => {
-        const accessToken = data.access_token;
-        
-        // Use the access token to make API requests and retrieve artist data
-        getArtistData(accessToken);
-        getPlaylistData(accessToken);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-    }
+
+  // Clear the stored state
+  localStorage.removeItem('spotify_auth_state');
+
+  // Make a POST request to exchange the authorization code for an access token
+  var tokenUrl = 'https://accounts.spotify.com/api/token';
+  var data = {
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: redirectUri,
+    client_id: clientId,
+    client_secret: 'YOUR_CLIENT_SECRET' // Replace with your client secret
+  };
+
+  fetch(tokenUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response from the token endpoint
+    var accessToken = data.access_token;
+    var expiresIn = data.expires_in;
+
+    // Perform actions with the access token (e.g., call Spotify Web API)
+    console.log('Access Token:', accessToken);
+    console.log('Expires In:', expiresIn);
+  })
+  .catch(error => {
+    console.error('Error:', error);
   });
-  
-  function getArtistData(accessToken) {
-    // Make API request to retrieve user's library data
-    fetch('https://api.spotify.com/v1/me/tracks', {
-      headers: {
-        'Authorization': 'Bearer ' + accessToken
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        // Extract and display artist information in a list
-        const artistList = document.getElementById('artistList');
-        artistList.innerHTML = '<h2>Artists in Your Library:</h2><ul>';
-  
-        const artists = data.items.map(item => item.track.artists.map(artist => artist.name));
-        artists.forEach(artist => {
-          artistList.innerHTML += `<li>${artist}</li>`;
-        });
-  
-        artistList.innerHTML += '</ul>';
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+}
+
+// Generate a random string of a given length
+function generateRandomString(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    // Check if the access token exists in the query string
-    const queryParams = new URLSearchParams(window.location.search);
-    const accessToken = queryParams.get('access_token');
-  
-    if (accessToken) {
-      // Access token exists, directly call getArtistData
-      getArtistData(accessToken);
-    }
-  });
-  
+  return result;
+}
+
+// Call the handleCallback function when the page is loaded
+window.addEventListener('DOMContentLoaded', handleCallback);
