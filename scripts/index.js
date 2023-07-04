c76941e5ec422d20112e2ecb0cfcecd710d3faf3
AlexanderDuncan1 <hellstern@gmail.com>
2023-07-04 16:17:44 +1000

    simpleeststartingcode

--- scripts/index.js
+++ scripts/index.js
@@ -1,63 +1,59 @@
 const clientId = '07574a44bbef47ad9c5b4949cf020c29';
-const clientSecret = '3a121714103f4ebbbe8a1d88a0e5fa8c';
 const redirectUri = 'https://alexanderduncan1.github.io/Group1_Project/';
 
 function authenticate() {
   const state = generateRandomString(16);
   localStorage.setItem('spotify_auth_state', state);
 
-  // Construct the authorization URL
-  const authorizeUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&state=${state}`;
+   const authorizeUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&state=${state}`;
 
-  // Redirect the user to Spotify's authorization endpoint
+  // Redirect to Spotify's authorization endpoint
   window.location.href = authorizeUrl;
 }
-
-// Handle the callback logic
 function handleCallback() {
   // Extract the query parameters from the callback URL
   const query = window.location.search.substring(1);
   const params = new URLSearchParams(query);
 
-  // Retrieve the authorization code and state from the query parameters
+  // gets authorization code and state
   const code = params.get('code');
   const state = params.get('state');
 
-  // Verify the state to prevent CSRF attacks
+  // Verify the state ? and secures 
   const storedState = localStorage.getItem('spotify_auth_state');
-  localStorage.removeItem('spotify_auth_state'); // Remove the stored state to prevent reusing it
-
-  if (state === null || state !== storedState) {
+  if (!state || state !== storedState) {
     console.error('Invalid state parameter.');
     return;
   }
 
-  // Make a POST request to exchange the authorization code for an access token
+  // Clear the stored state ? not sure why, copy pasted from spotify dev api
+  localStorage.removeItem('spotify_auth_state');
+
+  // POST request to swamp the authorization for an access token
   const tokenUrl = 'https://accounts.spotify.com/api/token';
-  const data = new URLSearchParams({
+  const data = {
     grant_type: 'authorization_code',
     code: code,
     redirect_uri: redirectUri,
     client_id: clientId,
-    client_secret: clientSecret
-  });
+    client_secret: '3a121714103f4ebbbe8a1d88a0e5fa8c'
+  };
 
   fetch(tokenUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: data
+    body: new URLSearchParams(data)
   })
     .then(response => response.json())
     .then(data => {
-      // Handle the response from the token endpoint
+      // response from the token endpoint
       const accessToken = data.access_token;
       const expiresIn = data.expires_in;
 
       // Perform actions with the access token (e.g., call Spotify Web API)
-      console.log('Access Token:', accessToken);
-      console.log('Expires In:', expiresIn);
+      // Replace this comment with your desired code
     })
     .catch(error => {
       console.error('Error:', error);
@@ -76,4 +72,3 @@ function generateRandomString(length) {
 
 // Call the handleCallback function when the page is loaded
 window.addEventListener('DOMContentLoaded', handleCallback);
-
